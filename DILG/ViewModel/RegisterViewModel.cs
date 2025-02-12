06c2914d8df25c4aefcafc85c974ed873a2dcd82
Navarone <Navarone@NAV-PC>
2025-02-12 12:27:19 +0800

    Add project files.Initial commit

---
+++ DILG/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DILG.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "FullName is required.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "UserName is required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Contact Number is required.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Position is required.")]
+        public string Position { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
